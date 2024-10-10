2ee153b0fd3fb38debe736cf918e93e5b7b28a71
Francesco Villa <fravilla30@gmail.com>
2024-10-10 11:48:57 +0200

    remove useless scripts

--- printer.py
+++
@@ -1,8 +0,0 @@
-import detectorlib
-
-path="2022_LOU_AZE_field_maturation_final_matrix.xlsx"
-path2='2021_All raw data_No_background.xlsx'
-graph=detectorlib.printer()
-graph.load(path2, 6)
-
-graph.print_all()
